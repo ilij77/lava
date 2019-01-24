0f09fd4c24885ea94eeb3de56314b7a35a03bfa8
Iliya <ilij@ya.ru>
2019-01-24 03:14:33 +0300

    День 1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Adverts</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,14 +19,15 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+<body id="app">
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Adverts
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -71,10 +72,25 @@
                 </div>
             </div>
         </nav>
+    </header>
 
-        <main class="py-4">
+        <main class="app-content py-3">
+            <div class="container">
+
             @yield('content')
+            </div>
         </main>
-    </div>
+
+    <footer>
+
+        <div class="container">
+            <div class="border-top pt-3">
+                <p>&copy;{{date('Y')}} - Adverts</p>
+            </div>
+        </div>
+
+    </footer>
+
+
 </body>
 </html>
